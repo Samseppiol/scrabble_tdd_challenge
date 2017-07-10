9f0b8f26b4b6fffa7ca4baa2d020d95edba468e7
Samseppiol <j_hage@live.com>
2017-07-10 11:37:57 +1000

    Refactored. Got rid of obsolete code. Wrote new tests to describe the desired behaviour of methods, not the logic to be inputted. Changed values to a constant instead of a method

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,30 +1,25 @@
 class Scrabble
 
-  def values 
-    {
-      "A" => 1, "B" => 3, "C" => 3, "D" => 2,
-      "E" => 1, "F" => 4, "G" => 2, "H" => 4,
-      "I" => 1, "J" => 8, "K" => 5, "L" => 1,
-      "M" => 3, "N" => 1, "O" => 1, "P" => 3,
-      "Q" => 10, "R" => 1, "S" => 1, "T" => 1,
-      "U" => 1, "V" => 4, "W" => 4, "X" => 8,
-      "Y" => 4, "Z" => 10
-    }
-  end
+  VALUES = {
+    "A" => 1, "B" => 3, "C" => 3, "D" => 2,
+    "E" => 1, "F" => 4, "G" => 2, "H" => 4,
+    "I" => 1, "J" => 8, "K" => 5, "L" => 1,
+    "M" => 3, "N" => 1, "O" => 1, "P" => 3,
+    "Q" => 10, "R" => 1, "S" => 1, "T" => 1,
+    "U" => 1, "V" => 4, "W" => 4, "X" => 8,
+    "Y" => 4, "Z" => 10
+  }
 
   def score(word)
-    word = word.to_s
-    if word == nil
-      total =+ 0
-    else 
+    total = 0
+    unless word == nil
       jumble = word.upcase.split('')
-      total = 0
       jumble.each do |char|
-        total+= values[char]
+        total += VALUES[char]
       end 
-      total
     end
-  end 
+    total
+  end
 
   def word_xtwo(total)
     total = total.to_i
@@ -36,16 +31,15 @@ class Scrabble
     total * 3
   end 
 
-  def start 
-    game = Scrabble.new
+  def start
     puts "Enter a word"
     input = gets.chomp
-    final_score = game.score(input)
+    final_score = self.score(input)
     puts "Your scrabble score for the word: #{input} is #{final_score}"
     sleep 1
-    puts "Your score multiplied by two is #{game.word_xtwo(final_score)}"
+    puts "Your score multiplied by two is #{self.word_xtwo(final_score)}"
     sleep 1
-    puts "Your score multiplied by three is #{game.word_xthree(final_score)}"
+    puts "Your score multiplied by three is #{self.word_xthree(final_score)}"
     puts "Choose another word? Y/N"
     choice = gets.chomp.upcase
     if choice.include? "Y"
--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -4,82 +4,65 @@ RSpec.describe 'A game of scrabble' do
   let(:scrabble) {Scrabble.new}
 
   it 'should have a set of values stored in a hash' do
-    expect(scrabble.values).to be_kind_of(Hash)
+    expect(Scrabble::VALUES).to be_kind_of(Hash)
   end
 
-  it 'should have a score calculator, that is passed an object' do 
-    word = scrabble.score('example')
-    expect(word).to be_kind_of(Object)
-  end  
-
-  it 'should have a multiply by two ability, that is passed an integer' do 
-    number = scrabble.word_xtwo(4)
-    expect(number).to be_kind_of(Integer)
-  end 
-
-  it 'should have a multiply by three ability that is passed an integer' do 
-    number = scrabble.word_xthree(4)
-    expect(number).to be_kind_of(Integer)
-  end  
-  
-end 
-
-scrabble = Scrabble.new
-
-RSpec.describe 'the values hash' do 
-  let(:values) {scrabble.values}
-
-  it 'should include letters of the alphabet as a key' do 
-    expect(values).to include("A", "B", "Y", "Z")
-    # Slightly different syntax, achieving the same thing.
-    expect(values).to have_key("Q")
-  end 
-
-  it 'should have values corresponding to the letters' do
-    expect(values["A"]).to eq(1)
-    expect(values["K"]).to eq(5)
-    expect(values["Z"]).to eq(10)
-  end 
-end 
-
-RSpec.describe 'the scrabble scoring system' do 
-  word = 'example'
-  jumble = word.upcase.split('')
-  total = 0
-  let(:values) {scrabble.values}
-
-  it 'should split the word in to separate capital letters' do 
-    expect(jumble).to eq(["E", "X", "A", "M", "P", "L", "E"])
-  end
-
-  it 'should have a total that begins at 0 and can increment only positively' do 
-    expect(total).to be >= 0 
-  end 
-
-  it 'should loop over the given letters and add all values together' do 
-    jumble.each do |char|
-      total += values[char]
-    end 
+  it 'should have a score calculator, that returns a score for any given word' do 
+    total = scrabble.score('example')
     expect(total).to eq(18)
+
+    total = scrabble.score('cabbage')
+    expect(total).to eq(14)
+
+    total = scrabble.score('hello')
+    expect(total).to eq(8)
+
+    # Test individual letters match their corresponding numeric value
+    total = scrabble.score('A')
+    expect(total).to eq(1)
+
+    total = scrabble.score('K')
+    expect(total).to eq(5)
+
+    total = scrabble.score('Z')
+    expect(total).to eq(10)
   end 
 
-end
+  it 'should return a score of 0 for nil or an empty string' do 
+    # test empty string and nil
+    total = scrabble.score('')
+    expect(total).to eq(0)
 
-RSpec.describe 'the word multipliers' do 
-  score = 5
-
-  context 'when multiplying by two' do 
-    it 'should double the given score' do 
-      x_two = score * 2
-      expect(x_two).to eq(10)
-    end
+    total = scrabble.score(nil)
+    expect(total).to eq(0)
   end 
 
-  context 'when multiplying by three' do 
-    it 'should triple the given score' do 
-      x_three = score * 3 
-      expect(x_three).to eq(15)
-    end 
+  it 'should have a multiply word by two ability' do 
+    number = scrabble.word_xtwo(4)
+    expect(number).to eq(8)
+
+    number = scrabble.word_xtwo(9)
+    expect(number).to eq(18)
+
+    number = scrabble.word_xtwo('')
+    expect(number).to eq(0)
+
+    number = scrabble.word_xtwo(nil)
+    expect(number).to eq(0)
+  end 
+
+  it 'should have a multiply word by three ability' do 
+    number = scrabble.word_xthree(4)
+    expect(number).to eq(12)
+
+    number = scrabble.word_xthree(9)
+    expect(number).to eq(27)
+
+    number = scrabble.score('')
+    expect(number).to eq(0)
+
+    number = scrabble.word_xthree(nil)
+    expect(number).to eq(0)
   end
-  
+
 end 
